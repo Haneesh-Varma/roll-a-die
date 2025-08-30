1cfb11a627ba4237b435ffeae563225767c6a9cd
K Haneesh Varma <haneeshvarma@gmail.com>
2025-08-30 11:06:17 +0530

    Update rolling_dice.py

--- rolling_dice.py
+++ rolling_dice.py
@@ -2,7 +2,7 @@ import random
 while True:
     user_imp = input("Roll the dice(yes/no)?: ").lower()
     if user_imp == "yes":
-        print(f"The number you've rolled is,{random.randint(1,7)}")
+        print(f"The number you've rolled is,{random.randint(1,6)}")
     elif user_imp == "no":
         print("Thanks for playing!")
         break
